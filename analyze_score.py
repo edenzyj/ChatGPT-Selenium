7bf1ddbc65eb6748e5eec0dc9c9133e5ca933cee
edenzyj <edenzyj88@gmail.com>
2024-11-02 00:59:19 +0800

    automatically calculate score

--- analyze_score.py
+++ analyze_score.py
@@ -1,8 +1,6 @@
-import numpy as np
-
 comparison_folder = "output_file/1st_answers/"
 
-comparison_file = "llama3.2_llama3_comparison_revised.txt"
+comparison_file = "llama3.1_llama3_comparison_revised.txt"
 
 first_scores = []
 second_scores = []
@@ -20,18 +18,18 @@ with open(comparison_folder + comparison_file, "r") as fr:
         
         score_list = comparison_str.split("/100")
         
-        first_scores.append(int(score_list[int(len(score_list)//2)-1].split(" ")[-1]))
-        second_scores.append(int(score_list[-2].split(" ")[-1]))
+        first_scores.append(float(score_list[int(len(score_list)//2)-1].split(" ")[-1]))
+        second_scores.append(float(score_list[-2].split(" ")[-1]))
         
     fr.close()
 
 first_avr = sum(first_scores) / len(first_scores)
 second_avr = sum(second_scores) / len(second_scores)
 
-first_rms = np.sqrt(np.mean(score**2 for score in first_scores))
-second_rms = np.sqrt(np.mean(score**2 for score in second_scores))
+first_mse = sum([(score - first_avr)**2 for score in first_scores]) / len(first_scores)
+second_mse = sum([(score - second_avr)**2 for score in second_scores]) / len(second_scores)
 
 print("First Average : {}".format(first_avr))
-print("First RMS : {}".format(first_rms))
+print("First MSE : {}".format(first_mse))
 print("Second Average : {}".format(second_avr))
-print("Second RMs : {}".format(second_rms))
+print("Second MSE : {}".format(second_mse))
