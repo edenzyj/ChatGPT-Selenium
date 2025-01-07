6f0a0bf07e559e62ebd6ed5bd01e4450427f55d8
zyj@MS <edenzyj88@gmail.com>
2025-01-07 22:08:33 +0800

    apply analyzing score to output file with json format

---
+++ analyze_score_json.py
@@ -0,0 +1,42 @@
+from cmath import sqrt
+import json
+
+comparison_folder = "output_file/9907/"
+
+comparison_file = "no_reference_vs_reference.json"
+
+first_scores = []
+second_scores = []
+
+first_win = 0
+second_win = 0
+
+with open(comparison_folder + comparison_file, "r") as fr:
+    data = json.load(fr)
+    
+    for item in data:
+        first_score = item['First_Score']
+        second_score = item['Second_Score']
+        
+        first_scores.append(first_score)
+        second_scores.append(second_score)
+        
+        if first_score > second_score:
+            first_win = first_win + 1
+        else:
+            second_win = second_win + 1
+    
+    fr.close()
+    
+first_avr = sum(first_scores) / len(first_scores)
+second_avr = sum(second_scores) / len(second_scores)
+
+first_vd = sqrt(sum([(score - first_avr)**2 for score in first_scores]) / len(first_scores))
+second_vd = sqrt(sum([(score - second_avr)**2 for score in second_scores]) / len(second_scores))
+
+print("First Win : {}".format(first_win))
+print("First Average : {}".format(first_avr))
+print("First VD : {}".format(first_vd))
+print("Second Win : {}".format(second_win))
+print("Second Average : {}".format(second_avr))
+print("Second VD : {}".format(second_vd))
