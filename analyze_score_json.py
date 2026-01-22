8122dae253bb0b574f2efe6f6e3b0bf50f7ec696
edenzyj <edenzyj88@gmail.com>
2026-01-23 06:52:13 +0800

    Add exception and question type in analysis method

--- analyze_score_json.py
+++ analyze_score_json.py
@@ -3,9 +3,16 @@ from scipy.stats import norm
 import numpy as np
 import json
 
+question_type_file = "output_file/question_type_1000.json"
+
+question_type = []
+
+with open(question_type_file, "r") as fq:
+    question_type = json.load(fq)
+
 comparison_folder = "output_file/RAG_Flow/"
 
-comparison_file = "RAGFlow_pure-dsr1-70b_comparison_100ex.json"
+comparison_file = "RAGFlow_pure-llama32-3b_comparison.json"
 
 first_scores = []
 second_scores = []
@@ -13,30 +20,36 @@ second_scores = []
 first_win = 0
 second_win = 0
 
-with open(comparison_folder + comparison_file, "r") as fr:
-    data = json.load(fr)
-    
-    for item in data:
-        qid = item['qid']
-        split_list = item['comparison'].split("/100")
-        
-        if len(split_list) < 3:
-            print(f"The comparison doesn't include both scores, qid = {qid}")
-            continue
-        
-        first_score = int(split_list[0].split(" ")[-1])
-        second_score = int(split_list[-2].split(" ")[-1])
-        
-        first_scores.append(first_score)
-        second_scores.append(second_score)
-        
-        if first_score > second_score:
-            first_win = first_win + 1
-        else:
-            second_win = second_win + 1
-    
-    fr.close()
-    
+try:
+    with open(comparison_folder + comparison_file, "r") as fr:
+        data = json.load(fr)
+
+        for item in data:
+            qid = item['qid']
+
+            if question_type[qid]['query_type'] == "Practice-related": continue
+
+            split_list = item['comparison'].split("/100")
+
+            if len(split_list) < 3:
+                print(f"The comparison doesn't include both scores, qid = {qid}")
+                continue
+
+            first_score = int(split_list[0].split(" ")[-1])
+            second_score = int(split_list[-2].split(" ")[-1])
+
+            first_scores.append(first_score)
+            second_scores.append(second_score)
+
+            if first_score > second_score:
+                first_win = first_win + 1
+            else:
+                second_win = second_win + 1
+
+        fr.close()
+except Exception as e:
+    print(f"An error occurred: {e}")
+
 first_avr = sum(first_scores) / len(first_scores)
 second_avr = sum(second_scores) / len(second_scores)
 
